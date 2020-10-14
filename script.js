2e14b457acbde58c67d99768e9b05c9c21bfaa0b
febby <gunawan.febby@gmail.com>
2020-10-14 22:38:56 +0700

    Added function to load API

---
+++ script.js
@@ -0,0 +1,32 @@
+const videoElement = document.getElementById('video');
+const PipButton = document.getElementById('btn');
+const loadButton = document.getElementById('load-video')
+
+// Prompt user to select media stream, pass to video element, then play
+async function selectMediaStream(){
+    try {
+        // console.log("Running")
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+
+    } catch (error) {
+        console.log('Whoops,theres an error:', error);
+    }
+}
+
+
+PipButton.addEventListener('click', async () => {
+    //disable button
+    PipButton.disabled = true;
+    // Start Picture in picture
+    await videoElement.requestPictureInPicture();
+    // Reset button
+    PipButton.disabled = false;
+});
+//on Load
+
+loadButton.addEventListener('click', () => {selectMediaStream();})
